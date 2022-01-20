4f150fa8d0ee3a67a739ebdfacd5a20085ee3a4e
adnankhateeb <adnankhateeb21@gmail.com>
2022-01-20 15:04:48 +0530

    created womenspage

---
+++ womensData2.js
@@ -0,0 +1,478 @@
+var womensData = [
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1896048781.jpg?size=400x400',
+      title: 'Grunt Style Dad Flag T-Shirt - Black ',
+      price: 66.72,
+      type: 'tShirt',
+      cashBack: 2,
+      percentCashBack: 2
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1786407843.jpg?size=400x400',
+      title: "Grunt Style Women's OMG Becky Look at His Beard T-Shirt - Purple ",
+      price: 8.93,
+      type: 'tShirt',
+      cashBack: 1,
+      percentCashBack: 7
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1896923943.jpg?size=400x400',
+      title: "Booty Lift Pocket - Black Solid FeatherSoft Women's Leggings ",
+      price: 98.85,
+      type: 'leggings',
+      cashBack: 9,
+      percentCashBack: 9
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1861847112.jpg?size=400x400',
+      title: "Grunt Style Women's Blue Line Crest T-Shirt - Black ",
+      price: 55.22,
+      type: 'tShirt',
+      cashBack: 1,
+      percentCashBack: 1
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1861847133.jpg?size=400x400',
+      title: 'Grunt Style Blue Line Crest T-Shirt - Black ',
+      price: 83.03,
+      type: 'tShirt',
+      cashBack: 7,
+      percentCashBack: 9
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289943/products/1907412872.jpg?size=400x400',
+      title: 'misc',
+      price: 29.22,
+      cashBack: 5,
+      percentCashBack: 14
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1896048809.jpg?size=400x400',
+      title: 'Grunt Style Born To Grill T-Shirt - Military Green ',
+      price: 29.71,
+      type: 'tShirt',
+      cashBack: 6,
+      percentCashBack: 20
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/243400/243406/products/1865791825.jpg?size=400x400',
+      title: "Inc Bow-Back Shift Dress, Created for Macy's ",
+      price: 32.95,
+      type: 'dress',
+      cashBack: 2,
+      percentCashBack: 4
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1872684450.jpg?size=400x400',
+      title: 'Grunt Style Realtree Edge Rifle Flag Pullover Hoodie - Black ',
+      price: 9.22,
+      type: 'hoodie',
+      cashBack: 1,
+      percentCashBack: 3
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1896048783.jpg?size=400x400',
+      title: 'Grunt Style Dad Defined T-Shirt - Heather Gray ',
+      price: 30.81,
+      type: 'tShirt',
+      cashBack: 5,
+      percentCashBack: 15
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/280600/280604/products/1902319177.jpg?size=400x400',
+      title: "PJ Couture Women's Plus Size Patterned Jogger and Raglan Top Long Pajama Set ",
+      price: 85.89,
+      type: 'pajama',
+      cashBack: 6,
+      percentCashBack: 7
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/269300/269358/products/1914798397.jpg?size=400x400',
+      title: 'Peanuts Charlie Brown Snoopy and Woodstock Sueded Fleece Sleep Pants ',
+      price: 71.67,
+      type: 'pants',
+      cashBack: 1,
+      percentCashBack: 1
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1891547505.jpg?size=400x400',
+      title: 'Grunt Style Fck Your Feelings Pullover Hoodie - Military Green ',
+      price: 35.07,
+      type: 'hoodie',
+      cashBack: 3,
+      percentCashBack: 9
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1865620730.jpg?size=400x400',
+      title: "Grunt Style Women's Realtree Xtra Sea Glass Mama Bear T-Shirt - Black ",
+      price: 88.41,
+      type: 'tShirt',
+      cashBack: 4,
+      percentCashBack: 4
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1877396174.jpg?size=400x400',
+      title: 'misc',
+      price: 89.03,
+      cashBack: 4,
+      percentCashBack: 5
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289943/products/1907393978.jpg?size=400x400',
+      title: 'misc',
+      price: 86.35,
+      cashBack: 6,
+      percentCashBack: 7
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1880097218.jpg?size=400x400',
+      title: 'misc',
+      price: 78.61,
+      cashBack: 16,
+      percentCashBack: 20
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289927/products/1908032977.jpg?size=400x400',
+      title: 'Rainbow Striped with Animal Paw Prints Ankle Socks (Adult Medium) ',
+      price: 65.74,
+      type: 'sock',
+      cashBack: 8,
+      percentCashBack: 12
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289927/products/1908033137.jpg?size=400x400',
+      title: 'Colorful Purple and Pink Wavy Pattern Socks ',
+      price: 14.96,
+      type: 'sock',
+      cashBack: 1,
+      percentCashBack: 6
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1907996998.jpg?size=400x400',
+      title: "Solid Caramel Fleece FeatherSoft Women's Leggings ",
+      price: 87.39,
+      type: 'leggings',
+      cashBack: 13,
+      percentCashBack: 15
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1909562249.jpg?size=400x400',
+      title: 'Christmas Lights - Two Pockets Holiday Wide Band Leggings ',
+      price: 61.71,
+      type: 'leggings',
+      cashBack: 4,
+      percentCashBack: 6
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/243400/243406/products/1834684579.jpg?size=400x400',
+      title: 'misc',
+      price: 62.15,
+      cashBack: 4,
+      percentCashBack: 6
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/269300/269358/products/1914798391.jpg?size=400x400',
+      title: 'Disney Mickey Mouse Kicked Back Pose Striped Fashion Dress ',
+      price: 23.89,
+      type: 'dress',
+      cashBack: 4,
+      percentCashBack: 14
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1909562243.jpg?size=400x400',
+      title: 'Golden Tree - Two Pockets Wide Band Holiday / Christmas Wide Band Leggings ',
+      price: 12.58,
+      type: 'leggings',
+      cashBack: 1,
+      percentCashBack: 8
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927095.jpg?size=400x400',
+      title: "Lively Swirls - Pockets Top Quality Women's Fitness ",
+      price: 62.01,
+      type: 'top',
+      cashBack: 9,
+      percentCashBack: 14
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927854.jpg?size=400x400',
+      title: "Layla - Maroon Top Quality Fashion Women's Top ",
+      price: 45.54,
+      type: 'top',
+      cashBack: 7,
+      percentCashBack: 14
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/249300/249385/products/1896048777.jpg?size=400x400',
+      title: 'Grunt Style Iron Medic T-Shirt - Black ',
+      price: 38.34,
+      type: 'tShirt',
+      cashBack: 8,
+      percentCashBack: 20
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1909562255.jpg?size=400x400',
+      title: 'Solid Black - Two Pockets - Wide Band ButterSoft Leggings ',
+      price: 1.26,
+      type: 'leggings',
+      cashBack: 1,
+      percentCashBack: 12
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1893549602.jpg?size=400x400',
+      title: "Solid Black Jogger FeatherSoft Women's Pants ",
+      price: 60.3,
+      type: 'pants',
+      cashBack: 8,
+      percentCashBack: 13
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927131.jpg?size=400x400',
+      title: "Solid Black FeatherSoft Women's Leggings ",
+      price: 58.23,
+      type: 'leggings',
+      cashBack: 5,
+      percentCashBack: 8
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289943/products/1907398848.jpg?size=400x400',
+      title: 'Disney Womens/Ladies Daisy Duck Cotton Pajama Set ',
+      price: 6.46,
+      type: 'pajama',
+      cashBack: 1,
+      percentCashBack: 4
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1882034949.jpg?size=400x400',
+      title: 'BCBGMAXAZRIA Womens Sleeveless Cape Jacket ',
+      price: 9.48,
+      type: 'jacket',
+      cashBack: 2,
+      percentCashBack: 17
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927186.jpg?size=400x400',
+      title: "Floral Forest Printed FeatherSoft Women's Leggings ",
+      price: 0.23,
+      type: 'leggings',
+      cashBack: 1,
+      percentCashBack: 19
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927761.jpg?size=400x400',
+      title: "Ruby Houndstooth Printed FeatherSoft Women's Leggings ",
+      price: 79.36,
+      type: 'leggings',
+      cashBack: 7,
+      percentCashBack: 9
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1893108937.jpg?size=400x400',
+      title: "Exquisite Printed FeatherSoft Women's Leggings ",
+      price: 15.54,
+      type: 'leggings',
+      cashBack: 2,
+      percentCashBack: 8
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1892927626.jpg?size=400x400',
+      title: "Pretty Flowers Printed FeatherSoft Women's Leggings ",
+      price: 26.27,
+      type: 'leggings',
+      cashBack: 3,
+      percentCashBack: 10
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/288000/288080/products/1893108936.jpg?size=400x400',
+      title: "Amazing Printed FeatherSoft Women's Leggings ",
+      price: 73.2,
+      type: 'leggings',
+      cashBack: 2,
+      percentCashBack: 2
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1805918202.jpg?size=400x400',
+      title: 'Karen Kane Womens Plaid Mid-Length Motorcycle Jacket ',
+      price: 23.04,
+      type: 'jacket',
+      cashBack: 2,
+      percentCashBack: 8
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1865812743.jpg?size=400x400',
+      title: 'Jessica Simpson Womens Fleece Water Resistant Soft Shell Jacket ',
+      price: 58.83,
+      type: 'jacket',
+      cashBack: 8,
+      percentCashBack: 13
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1910762215.jpg?size=400x400',
+      title: 'Big Chill Womens Sherpa Hooded Fleece Jacket ',
+      price: 65.71,
+      type: 'jacket',
+      cashBack: 8,
+      percentCashBack: 11
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1869550697.jpg?size=400x400',
+      title: 'Love + Joy by FCT Womens Winter Faux Fur Jacket ',
+      price: 97.67,
+      type: 'jacket',
+      cashBack: 10,
+      percentCashBack: 10
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/264900/264928/products/1891421676.jpg?size=400x400',
+      title: "Nike Women's Sportswear Shine Metallic-Graphic Half-Zip Top ",
+      price: 11.1,
+      type: 'top',
+      cashBack: 2,
+      percentCashBack: 17
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/269300/269358/products/1909366702.jpg?size=400x400',
+      title: 'Marvel Brand Text Puff Print Hoodie With Character Line Up Back Print ',
+      price: 93.92,
+      type: 'hoodie',
+      cashBack: 5,
+      percentCashBack: 5
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289943/products/1907413145.jpg?size=400x400',
+      title: 'A&R Towels Womens/Ladies Waffle Hooded Bathrobe ',
+      price: 22.53,
+      type: 'bathrobe',
+      cashBack: 1,
+      percentCashBack: 0
+    },
+    {
+      link: 'https://img.shop.com/Image/250000/253200/253251/products/1825248728.jpg?size=400x400',
+      title: 'Free People Womens Popcorn Pullover Sweater ',
+      price: 54.94,
+      type: 'sweater',
+      cashBack: 4,
+      percentCashBack: 7
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/278100/278132/products/1920810833.jpg?size=400x400',
+      title: 'MICHAEL KORS Womens Black Belted Raincoat Coat ',
+      price: 55.99,
+      type: 'coat',
+      cashBack: 4,
+      percentCashBack: 7
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289927/products/1908032836.jpg?size=400x400',
+      title: 'Heart Pattern Sock (Love) ',
+      price: 57.6,
+      type: 'sock',
+      cashBack: 12,
+      percentCashBack: 20
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/278100/278132/products/1920267639.jpg?size=400x400',
+      title: 'LONDON FOG Womens Beige Belted Trench Coat ',
+      price: 14.1,
+      type: 'coat',
+      cashBack: 2,
+      percentCashBack: 12
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/248000/248061/products/1859557356.jpg?size=400x400',
+      title: 'misc',
+      price: 62.16,
+      cashBack: 6,
+      percentCashBack: 9
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/269300/269358/products/1877161839.jpg?size=400x400',
+      title: 'Busch Light Beer Logo White Colorway Hoodie ',
+      price: 34.01,
+      type: 'hoodie',
+      cashBack: 6,
+      percentCashBack: 15
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/287200/287278/products/1890508827.jpg?size=400x400',
+      title: 'Cozy Soft Comfortable Sherpa Hoodie Sweatshirt - Oversized Blanket Sweatshirt Hoody - Hooded, Large Pocket, Reversible ',
+      price: 82.76,
+      type: 'hoodie',
+      cashBack: 16,
+      percentCashBack: 19
+    },
+    {
+      link: 'https://img.shop.com/Image/260000/269300/269358/products/1867144984.jpg?size=400x400',
+      title: 'My Hero Academia Chibi All Over Print Sleep Pants ',
+      price: 92.31,
+      type: 'pants',
+      cashBack: 4,
+      percentCashBack: 4
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289900/289943/products/1907420755.jpg?size=400x400',
+      title: 'Trespass Womens/Ladies Seawater Waterproof Jacket ',
+      price: 80.88,
+      type: 'jacket',
+      cashBack: 1,
+      percentCashBack: 1
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/273200/273200/products/1874307409.jpg?size=400x400',
+      title: 'Rag & Bone Womens Ribbed Trim Detailed Crewneck Sweater ',
+      price: 9.75,
+      type: 'sweater',
+      cashBack: 1,
+      percentCashBack: 6
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/248000/248061/products/1859556202.jpg?size=400x400',
+      title: "Blue and Yellow Need Vacation Women's Adult V-Neck Sleep Shirt - Extra Large ",
+      price: 51.23,
+      type: 'shirt',
+      cashBack: 7,
+      percentCashBack: 13
+    },
+    {
+      link: 'https://img.shop.com/Image/240000/248000/248061/products/1859556888.jpg?size=400x400',
+      title: "Need More Cocktails V-Neck Women's Adult Sleep Shirt - Extra Large ",
+      price: 46.33,
+      type: 'shirt',
+      cashBack: 3,
+      percentCashBack: 6
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/278100/278132/products/1917677906.jpg?size=400x400',
+      title: 'ADRIANNA PAPELL Womens Red Sleeveless Asymmetrical Neckline Midi Hi-Lo Cocktail Dress ',
+      price: 27.4,
+      type: 'dress',
+      cashBack: 4,
+      percentCashBack: 13
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/280600/280604/products/1879389979.jpg?size=400x400',
+      title: "Fruit of the Loom Women's Plus Fit for Me White Cotton Brief Underwear (6 Pack) ",
+      price: 74.31,
+      type: 'underwear',
+      cashBack: 8,
+      percentCashBack: 10
+    },
+    {
+      link: 'https://img.shop.com/Image/280000/289600/289605/products/1907969553.jpg?size=400x400',
+      title: 'Dolce & Gabbana Black Polka Dots Casual Trouser Pants ',
+      price: 75.98,
+      type: 'pants',
+      cashBack: 11,
+      percentCashBack: 14
+    },
+    {
+      link: 'https://img.shop.com/Image/270000/278100/278132/products/1918439581.jpg?size=400x400',
+      title: 'INC Womens Orange Ruched Short Sleeve Crew Neck Top ',
+      price: 0.05,
+      type: 'top',
+      cashBack: 0,
+      percentCashBack: 0
+    }
+  ];
+
+localStorage.setItem("womenData", JSON.stringify(womensData));
